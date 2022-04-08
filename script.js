efa4dd819ea5de590db315c91f1123aed52729fc
vmoreirav <vmoreira.m09@gmail.com>
2022-04-07 21:21:35 -0300

    stuck in the appendNumber

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 class Calculator {
-    constructor()
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+    }
 }
 
 
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const numberButtons = document.querySelectorAll('[data-number]') //check
+const operationButtons = document.querySelectorAll('[data-operation]') //check
+const equalsButton = document.querySelector('[data-equals]') //check
+const deleteButton = document.querySelector('[data-delete]') //check
+const allClearButton = document.querySelector('[data-all-clear]') //check
+const previousOperandTextElement = document.querySelector('[data-previous-operand]') //check
+const currentOperandTextElement = document.querySelector('[data-current-operand]') //check
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement) //check
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+}) //check
